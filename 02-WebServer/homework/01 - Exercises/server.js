a0b0402f129fa12afef169c20358edb525d97b67
miEquipo <brasanluc123@gmail.com>
2023-04-11 17:52:03 -0300

    Creando-Mi-Propio-Server

--- 02-WebServer/homework/01 - Exercises/server.js
+++ 02-WebServer/homework/01 - Exercises/server.js
@@ -2,8 +2,40 @@ var fs = require("fs");
 var http = require("http");
 /* ⚠️ NO MODIFICAR NADA POR ENCIMA DE ESTA LÍNEA ⚠️ */
 /* AQUÍ DEBAJO PUEDES ESCRIBIR LA CONSTANTE DEL PUERTO */
-const PORT = null;
-
+const PORT = 3001;
+http
+  .createServer((req, res) => {
+    console.log(`Server raised in port ${PORT}`);
+    if (req.url === "/api") {
+      fs.readFile("./utils/dogsData.json", (err, data) => {
+        if (data) {
+          res.writeHead(200, { "Content-Type": "application/json" });
+          res.end(data);
+        } else if (err) {
+          res.writeHead(404, { "Content-Type": "text/plain" });
+          res.end("json not found");
+        }
+      });
+      return;
+    }
+    if (req.url === "/allDogs") {
+      fs.readFile("./utils/allDogs.html", "utf-8", (err, data) => {
+        if (data) {
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.end(data);
+        } else if (err) {
+          res.writeHead(404, { "Content-Type": "text/plain" });
+          res.end("html not found");
+        }
+      });
+      return;
+    }
+    return (
+      res.writeHead(404, { "Content-Type": "text/plain" }),
+      res.end("Route not found")
+    );
+  })
+  .listen(PORT, "localhost");
 /* ⚠️ LA LÍNEA SIGUIENTE TIENE QUE QUEDAR COMO ESTÁ PARA PODER EXPORTAR EL SERVIDOR ⚠️ */
 module.exports =
   /* AQUÍ DEBAJO YA PUEDES ESCRIBIR TÚ CÓDIGO REEMPLAZANDO EL VALOR DE NULL POR EL SERVIDOR */
