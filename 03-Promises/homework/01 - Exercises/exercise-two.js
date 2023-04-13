2cabb738583c63974f73f6f820b3b65a17d4564d
miEquipo <brasanluc123@gmail.com>
2023-04-13 03:39:27 -0300

    Promises-homework

--- 03-Promises/homework/01 - Exercises/exercise-two.js
+++ 03-Promises/homework/01 - Exercises/exercise-two.js
@@ -8,7 +8,7 @@ let args = process.argv.slice(2).map(function (st) {
 
 module.exports = {
   problemAx: problemA,
-  problemBx: problemB
+  problemBx: problemB,
 };
 
 // corre cada problema dado como un argumento del command-line para procesar
@@ -19,15 +19,35 @@ args.forEach(function (arg) {
 
 function problemA() {
   // callback version
-  exerciseUtils.readFile("poem-two/stanza-01.txt", function (err, stanza) {
-    exerciseUtils.blue(stanza);
-  });
-  exerciseUtils.readFile("poem-two/stanza-02.txt", function (err, stanza) {
-    exerciseUtils.blue(stanza);
-  });
+  // exerciseUtils.readFile("poem-two/stanza-01.txt", function (err, stanza) {
+  // exerciseUtils.blue(stanza);
+  // });
+  // exerciseUtils.readFile("poem-two/stanza-02.txt", function (err, stanza) {
+  // exerciseUtils.blue(stanza);
+  // });
 
   // promise version
   // Tu código acá:
+
+  exerciseUtils
+    .promisifiedReadFile("poem-two/stanza-01.txt")
+    .then((respuesta) => {
+      exerciseUtils.blue(respuesta);
+    });
+  exerciseUtils
+    .promisifiedReadFile("poem-two/stanza-02.txt")
+    .then((respuesta) => {
+      exerciseUtils.blue(respuesta);
+      console.log("done");
+    });
+
+  // let promesa1 = exerciseUtils.promisifiedReadFile("poem-two/stanza-01.txt");
+  // let promesa2 = exerciseUtils.promisifiedReadFile("poem-two/stanza-02.txt");
+  // Promise.all([promesa1, promesa2]).then((data) => {
+  // exerciseUtils.blue(data[0]);
+  // exerciseUtils.blue(data[1]);
+  // console.log("done");
+  // });
 }
 
 function problemB() {
@@ -38,15 +58,28 @@ function problemB() {
   filenames[randIdx] = "wrong-file-name-" + (randIdx + 1) + ".txt";
 
   // callback version
-  filenames.forEach((filename) => {
-    exerciseUtils.readFile(filename, function (err, stanza) {
-      exerciseUtils.blue(stanza);
-      if (err) exerciseUtils.magenta(new Error(err));
-    });
-  });
+  // filenames.forEach((filename) => {
+  // exerciseUtils.readFile(filename, function (err, stanza) {
+  // exerciseUtils.blue(stanza);
+  // if (err) exerciseUtils.magenta(new Error(err));
+  // });
+  // });
 
   // promise version
   // Tu código acá:
+  let promesa = exerciseUtils.promisifiedReadFile(filenames[0]);
+  for (let i = 1; i <= filenames.length; i++) {
+    promesa = promesa.then((v) => {
+      exerciseUtils.blue(v);
+      return exerciseUtils.promisifiedReadFile(filenames[i]);
+    });
+    if (i === filenames.length) {
+      promesa.catch((error) => {
+        exerciseUtils.magenta(new Error(error));
+        console.log("done");
+      });
+    }
+  }
 }
 
 // EJERCICIO EXTRA
